80489daa19ea2a74713ce09aafe244940450dd5b
nunu <admjmadq@gmail.com>
2014-10-21 01:06:45 +0900

    キャラ位置の初期化

---
+++ Assets/script/chara_move.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class chara_move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Vector3[] vector = chara_preset_position.position_generator ();
+		transform.position = vector[0];
+		transform.eulerAngles = vector[1];
+//		transform.lossyScale = new Vector3(0.5f, 0.5f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/script/chara_preset_positions.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class chara_preset_position : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	static int position_number = 0;
+
+	public static Vector3[] position_generator (){
+		Vector3[] vector = new Vector3[2];
+		int position_repoint_number = 3;
+
+		vector[0].x = (float)(chara_preset_position.position_number % position_repoint_number);
+		vector[0].y = 1.55f;
+		vector[0].z = (float)(chara_preset_position.position_number / position_repoint_number);
+
+		vector [1].x = 20f;
+		vector [1].y = 0f;
+		vector [1].z = 0f;
+
+		chara_preset_position.position_number++;
+		return vector; 
+	}
+
+
+}
